55eb530176de0d2cc0eb28448c122b3c99050048
MichaelCho6556 <cho.michael13524@gmail.com>
2024-12-07 15:02:30 -0600

    finished?

---
+++ League Win Combinations/league-win-tracker/src/app/page.tsx
@@ -0,0 +1,9 @@
+import TeamTracker from "@/components/TeamTracker";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <TeamTracker />
+    </main>
+  );
+}
